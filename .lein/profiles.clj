3245746841d1b6a9fecb37a68de2a952b2019bb5
Bronsa <brobronsa@gmail.com>
2013-05-08 19:51:50 +0200

    updated .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:plugins [[bronsa/lein-swank "1.5.0"]
                   [lein-exec "0.2.1"]
+                  [lein-simpleton "1.1.0"]
                   [clj-stacktrace "0.2.5"]]}
- ;; :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
- ;;                                     'print-cause-trace)
- ;;                    new (ns-resolve (doto 'clj-stacktrace.repl require)
- ;;                                    'pst)]
- ;;                (alter-var-root orig (constantly @new)))]
- }
+ :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                     'print-cause-trace)
+                    new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                    'pst)]
+                (alter-var-root orig (constantly @new)))]}
