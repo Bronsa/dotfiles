c02c73ee57d730bb84402db9f25fdc7b26d439d4
Nicola Mometto <brobronsa@gmail.com>
2015-08-30 00:28:36 +0200

    update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,10 @@
 {:user {:plugins [[bronsa/lein-swank "1.5.0"]
-                  [lein-exec "0.2.1"]
-                  [lein-simpleton "1.1.0"]
-                  [clj-stacktrace "0.2.5"]]}
- :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                     'print-cause-trace)
-                    new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                    'pst)]
-                (alter-var-root orig (constantly @new)))]}
+                  [clj-stacktrace "0.2.5"]
+                  [lein-vanity "0.1.0"]
+                  [jonase/eastwood "0.2.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.9.1"]]
+        ;; :injections [(set! *print-level* 10)
+        ;;              (set! *print-length* 10)]
+        :repositorises [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
+                        ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]}}
---
+++ user.clj
@@ -0,0 +1,15 @@
+(in-ns 'clojure.core)
+
+(defn- print-object [o, ^Writer w]
+  (when (instance? clojure.lang.IMeta o)
+    (print-meta o w))
+  (.write w "#<")
+  (let [name (.getSimpleName (class o))]
+    (when (seq name) ;; anonymous classes have a simple name of ""
+      (.write w name)
+      (.write w " ")))
+  (.write w (str o))
+  (.write w ">"))
+
+(defmethod print-method Throwable [^Throwable o ^Writer w]
+  (print-object o w))
