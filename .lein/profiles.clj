151bfb1bf074da64daf8f52dd6889ceb1f386163
Nicola Mometto <brobronsa@gmail.com>
2015-09-13 16:26:52 +0200

    update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
-{:user {:plugins [[bronsa/lein-swank "1.5.0"]
-                  [clj-stacktrace "0.2.5"]
-                  [lein-vanity "0.1.0"]
-                  [jonase/eastwood "0.2.0-SNAPSHOT"]
-                  [refactor-nrepl "1.1.0"]
-                  [cider/cider-nrepl "0.9.1"]]
-        ;; :injections [(set! *print-level* 10)
-        ;;              (set! *print-length* 10)]
+{:user {:plugins [#_[cider/cider-nrepl "0.9.1"]
+                  #_[refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
+
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+        #_#_:injections [(set! *print-level* 10)
+                         (set! *print-length* 10)]
         :repositorises [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
                         ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]}}
